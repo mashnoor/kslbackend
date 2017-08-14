a03ada600ca3b143fd3932d158bb365891893e3b
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-08-14 23:35:01 +0600

    Neglecting Item detail file in subprocess

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ subprocess.call("python3 get_all_item_detail_link.py", shell=True)
 subprocess.call("python3 get_home_data.py", shell=True)
 subprocess.call("python3 get_item_detail.py", shell=True)
 subprocess.call("python3 get_latest_item_updates.py", shell=True)
-subprocess.call("python3 get_all_item_detail_link.py", shell=True)
+#subprocess.call("python3 get_all_item_detail_link.py", shell=True)
 subprocess.call("python3 get_market_movers.py", shell=True)
 
 
