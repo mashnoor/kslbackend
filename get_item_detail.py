d1831c7b5d47a5eaac54d2c3d16bd9ee9d8f22b1
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-08-15 14:59:20 +0600

    Written Script for getting item details

--- get_item_detail.py
+++ get_item_detail.py
@@ -1,28 +1,40 @@
-import requests, grequests
+import grequests, json
 
 from bs4 import BeautifulSoup
 from helpers import get_column_values
 
-item_detail_urls = ["http://www.cse.com.bd/companyDetails.php?scriptCode=QUJCQU5L"]
+with open("all_company_info.txt", "r") as f:
+    all_items_info = json.load(f)
+
+item_detail_urls = []
+item_company_names = []
+for item_info in all_items_info:
+    item_detail_urls.append(item_info['link'])
+    item_company_names.append(item_info['scd'])
+
+
+
 
 req = (grequests.get(url) for url in item_detail_urls)
 
 responses = grequests.map(req)
 
 
-soup = BeautifulSoup(responses[0].content, 'lxml')
-table_attrs = {"width":"100%", "border":"0", "cellpadding":"0", "cellspacing":"0"}
-table = soup.find_all("table", table_attrs)
 
-current_item_info = {}  # Store all the info of the company
-
-############# Parse Current Market Info Table ############
-
-inner_tables = table[2].find_all("table")
-get_column_values(str(inner_tables[0]), ['ltp', 'ltd', 'change', 'openprice', 'range'])
-get_column_values(str(inner_tables[1]), ['trade', 'volume', 'closeprice', 'ycp', 'capital'])
+for idx, response in enumerate(responses):
+    print item_company_names[idx]
 
 
-############ Parse Contract Info and Basic Info ##############
+    soup = BeautifulSoup(response.content, 'lxml')
+    table_attrs = {"width":"100%", "border":"0", "cellpadding":"0", "cellspacing":"0"}
+    table = soup.find_all("table", table_attrs)
 
-print get_column_values(str(table[5]), [1,2,3,4,5,6,7,8, 9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    current_item_info = {}  # Store all the info of the company
+
+    ############# Parse Current Market Info Table ############
+
+    inner_tables = table[2].find_all("table")
+    current_item_info.update(get_column_values(str(inner_tables[0]), ['ltp', 'ltd', 'change', 'openprice', 'range']))
+    current_item_info.update(get_column_values(str(inner_tables[1]), ['trade', 'volume', 'closeprice', 'ycp', 'capital']))
+    with open("item_details/" + item_company_names[idx] + ".txt", "w") as f:
+        f.write(json.dumps(current_item_info))
