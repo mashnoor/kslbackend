5be7541ef0af3b4a7590b75ce5dcdf5bbcd354c8
Nowfel Mashnoor <nmmashnoor@gmail.com>
2018-01-29 12:18:18 +0600

    Added financial ledger api

--- webhook/admin.py
+++ webhook/admin.py
@@ -1,7 +1,4 @@
-from flask import Flask, request, render_template
-
-
-
+from flask import Flask
 
 from requisitions import requisitions_api
 from accountrequests import account_request_api
@@ -11,8 +8,7 @@ from pyfcm import FCMNotification
 from orderstatusparse import getorderstatus_api
 from get_previous_day_end_data import previous_day_end_data_api
 from get_item_details import get_item_detail_api
-
-
+from financialledger import financial_ledger_api
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -24,6 +20,7 @@ app.register_blueprint(portfoliostatement_api)
 app.register_blueprint(getorderstatus_api)
 app.register_blueprint(previous_day_end_data_api)
 app.register_blueprint(get_item_detail_api)
+app.register_blueprint(financial_ledger_api)
 
 
 @app.route('/abc')
