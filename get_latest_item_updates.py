db6c5399441ce17beb1cf8b29e6daaeac2482b5b
Nowfel Mashnoor <nmmashnoor@gmail.com>
2018-01-31 21:07:48 +0600

    Changed a bit

--- get_latest_item_updates.py
+++ get_latest_item_updates.py
@@ -7,7 +7,8 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'lxml')
 
-table_attrs = {"id":"report", "width":"100%", "border":"0", "cellpadding":"0", "cellspacing":"0", "bgcolor":"#355DA2"}
+table_attrs = {"id": "report", "width": "100%", "border": "0", "cellpadding": "0", "cellspacing": "0",
+               "bgcolor": "#355DA2"}
 
 table = soup.find("table", table_attrs)
 all_items = []
@@ -16,11 +17,11 @@ for tr in table.find_all("tr"):
     curr_item = {}
     curr_item['item'] = td[1].get_text().strip()
     curr_item['ltp'] = td[2].get_text().strip()
-    curr_item['change'] = td[7].get_text().strip()
+    curr_item['changeval'] = td[7].get_text().strip()
     curr_item['volume'] = td[9].get_text().strip()
     all_items.append(curr_item)
 
-del(all_items[0])
+del (all_items[0])
 
 with open('all_items_latest_update.txt', 'w') as f:
     f.write(json.dumps(all_items))
