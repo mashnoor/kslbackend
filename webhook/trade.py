bf0fe537edfe78179c5dfa9597cde40c3f4a93e0
Nowfel Mashnoor <nmmashnoor@gmail.com>
2018-01-31 20:55:49 +0600

    Updated trading system

--- webhook/trade.py
+++ webhook/trade.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from flask import Blueprint, request
+import dbhelper
+
+trade_api = Blueprint('trade_api', __name__)
 
 url = "https://bangladeshstockmarket.com/LoadBalance.do"
 
@@ -8,10 +12,17 @@ order1url = "https://bangladeshstockmarket.com/Order1.do?exchangeSelected=CTG"
 verifyPassword = "https://bangladeshstockmarket.com/PasswordCheck.do"
 
 
-def buy_item(loginid, password, symbol, qty, price):
+@trade_api.route('/trade', methods=['POST'])
+def buy_item():
+    loginid = request.form.get('loginid')
+    password = request.form.get('password')
+    symbol = request.form.get('symbol')
+    qty = request.form.get('qty')
+    price = request.form.get('price')
+    verb = request.form.get('verb')
     d = {'loginid': loginid, 'password': password, 'lang1': 'default'}
     order1_data = {'spdBuySymbol': '', 'spdSellSymbol': '', 'submitbutton': 'Submit', 'exchange': 'CTG',
-                   'selectProduct': 'EQUITY', 'segment': 'DELIVERY', 'symbol': symbol, 'buysell': 'BUY',
+                   'selectProduct': 'EQUITY', 'segment': 'DELIVERY', 'symbol': symbol, 'buysell': verb,
                    'quantity': qty, 'referenceId': '', 'pricetype': 'LIMIT', 'price': price, 'triprice': '0',
                    'disquan': '', 'protectionPercent': '', 'filltype': 'GFD', 'minFillQty': '',
                    'expirydate': '', 'settlerid': '', 'priceViolation': 'N', 'lotsize': '1', 'userExchangeList': 'CTG',
@@ -24,7 +35,7 @@ def buy_item(loginid, password, symbol, qty, price):
         if (r.text == "NOTOK"):
             return "Login Failed"
         r = s.post(order1url, data=order1_data)
-        soup = BeautifulSoup(r.text, 'lxml')
+        soup = BeautifulSoup(r.text, 'html.parser')
         return soup.find_all('font')[0].text
         # with open('data.txt', 'w+') as f:
         #   f.write(r.text)
