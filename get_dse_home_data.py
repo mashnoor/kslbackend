f1e4743f58fb20dc92b8d657d5102bc712c26c9f
Nowfel Mashnoor <nmmashnoor@gmail.com>
2018-03-03 17:38:45 +0600

    Adde dses data

--- get_dse_home_data.py
+++ get_dse_home_data.py
@@ -19,6 +19,14 @@ result['dsex_data1'] = str(curr_divs[1].text).strip()
 result['dsex_data2'] = str(curr_divs[2].text).strip()
 result['dsex_data3'] = str(curr_divs[3].text).strip()
 
+#DSES
+curr_divs = alldivs[1].find_all('div')
+result['dses_data1'] = str(curr_divs[1].text).strip()
+result['dses_data2'] = str(curr_divs[2].text).strip()
+result['dses_data3'] = str(curr_divs[3].text).strip()
+
+
+
 # DS30
 curr_divs = alldivs[2].find_all('div')
 result['ds30_data1'] = str(curr_divs[1].text).strip()
