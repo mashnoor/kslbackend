af7ec98aa3bc34b5f60a974901999f20dde25f4f
Nowfel Mashnoor <nmmashnoor@gmail.com>
2019-01-16 14:45:01 +0600

    Changed the api section

--- webhook/ipoapi.py
+++ webhook/ipoapi.py
@@ -11,8 +11,8 @@ ipo_api = Blueprint('ipo_api', __name__)
 @ipo_api.route('/ipo')
 @flask_login.login_required
 def ipo():
-    # p = '/var/www/html/kslbackend/ksl_news.txt'
-    p = Path('../ipo.txt')
+    p = '/var/www/html/mash/kslbackend/ipo.txt'
+    #p = Path('../ipo.txt')
     with open(p) as f:
         content = f.read()
 
@@ -22,8 +22,7 @@ def ipo():
 @ipo_api.route('/updateipo', methods=['POST'])
 def updateipo():
     updatedIpo = request.form.get('ipo')
-    # p = '/var/www/html/kslbackend/ksl_news.txt'
-    p = Path('../ipo.txt')
+    p = '/var/www/html/mash/kslbackend/ipo.txt'
 
     with open(p, 'w') as f:
         f.write(str(updatedIpo).replace("\n", ""))
