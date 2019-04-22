8a9f817f5199d6469b0082db990c3e522d091633
Nowfel Mashnoor <nmmashnoor@gmail.com>
2019-04-22 12:09:21 +0600

    Added date in summary

--- get_dse_home_data.py
+++ get_dse_home_data.py
@@ -19,13 +19,11 @@ result['dsex_data1'] = str(curr_divs[1].text).strip()
 result['dsex_data2'] = str(curr_divs[2].text).strip()
 result['dsex_data3'] = str(curr_divs[3].text).strip()
 
-#DSES
+# DSES
 curr_divs = alldivs[1].find_all('div')
 result['dses_data1'] = str(curr_divs[1].text).strip()
 result['dses_data2'] = str(curr_divs[2].text).strip()
 result['dses_data3'] = str(curr_divs[3].text).strip()
-
-
 
 # DS30
 curr_divs = alldivs[2].find_all('div')
@@ -44,6 +42,10 @@ curr_divs = alldivs[6].find_all('div')
 result['issues_advanced'] = str(curr_divs[0].text).strip()
 result['issues_declined'] = str(curr_divs[1].text).strip()
 result['issues_unchanged'] = str(curr_divs[2].text).strip()
+
+# marketStatus
+marketstatus = soup.find("span", {"class": "time"})
+result['date'] = str(marketstatus.text).strip()
 
 with open('homedatas/dse_data.txt', 'w') as f:
     f.write(json.dumps(result))
