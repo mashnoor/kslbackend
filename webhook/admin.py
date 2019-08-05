91874afcdfdeb833cee473152ec9c701fcc37e1f
Nowfel Mashnoor <nmmashnoor@gmail.com>
2019-08-05 14:38:34 +0600

    Added separate file for host

--- webhook/admin.py
+++ webhook/admin.py
@@ -17,7 +17,7 @@ from loginapi import login_api
 import flask_login
 from kslnewsapi import kslnews_api
 from ipoapi import ipo_api
-
+import settings
 # Flask app should start in global layout
 app = Flask(__name__)
 
@@ -125,7 +125,7 @@ def notFound(error):
 
 
 if __name__ == '__main__':
-    port = 5003
+
     # print("Starting app on port %d" % port)
-    app.run(port=port, threaded=True)
+    app.run(host=settings.host, port=settings.port, threaded=True)
     # app.run(host='0.0.0.0', port=port)
